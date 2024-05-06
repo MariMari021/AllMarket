d044f902e1c808c68651c13e9a87bac16fc4b473
Giovanna Araujo <133662788+GigiAraujo@users.noreply.github.com>
2024-05-06 17:15:27 -0300

    quase pronto

--- routes.js
+++ routes.js
@@ -13,7 +13,7 @@ const Stack = createStackNavigator();
 
 const Navigation = () => {
   return (
-        <Stack.Navigator initialRouteName="Compras">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
           <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
           <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
