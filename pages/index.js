112388fd38df964d8a0e0eff44f08c8b12568def
MariMari021 <mariana.gsilva0021@gmail.com>
2024-05-06 17:07:40 -0300

    terminar cadastro produtos

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,329 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>Home!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+
+export function Home({navigation}) {
+    const [text, setText] = useState('');
+    const [fontsLoaded, fontError] = useFonts({ 'Inter': require('../assets/fonts/Inter-VariableFont_slnt,wght.ttf') });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
+    return (
+        <ScrollView>
+            <View style={styles.container} onLayout={onLayoutRootView}>
+                <View style={styles.header}>
+                    <View style={styles.headerInicio}>
+                        <Image
+                            style={styles.profile}
+                            source={require('../assets/profile.png')}
+                        />
+                        <Image
+                            style={styles.sacola}
+                            source={require('../assets/sacola.png')}
+                        />
+                    </View>
+                    <Text style={styles.bemVindo}>Seja bem-vindo ao <Text style={styles.ALLSpan}>ALL</Text><Text style={styles.marketSpan}>Market</Text></Text>
+                </View>
+
+                <View style={styles.precoMax}>
+                    <Image
+                        style={styles.dinheiro}
+                        source={require('../assets/imgValor.png')}
+                    />
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite o valor máx. da compra."
+                        placeholderTextColor="#000"
+                        onChangeText={setText}
+                        value={text}
+                    />
+                </View>
+                <View style={styles.mercados}>
+                    <Text style={styles.mercadosTitulo}>
+                        Mercados
+                    </Text>
+                    <TouchableOpacity style={styles.adicionarMercado}>
+                        <Text style={styles.adicionarMercadoTexto}>
+                            Adicionar <Text style={styles.adicionarMercadoSpan}>mercado</Text>
+                        </Text>
+                        <Image
+                            style={styles.adicionarIcon}
+                            source={require('../assets/adicionar.png')}
+                        />
+                    </TouchableOpacity>
+                    <View style={styles.opcoesMercado}></View>
+                </View>
+                <View style={styles.cardContainer}>
+                    <View style={styles.card}>
+                        <Image
+                            style={styles.imgCardAdicionar}
+                            source={require('../assets/imgCard1.png')}
+                        />
+                        <Text style={styles.tituloAdicionar}>
+                            Adicionar
+                        </Text>
+                        <Text style={styles.tituloAdicionarSpan}> o produto!</Text>
+                        <TouchableOpacity style={styles.botaoMais}  onPress={() => navigation.navigate('Produtos')}>
+                            <Image
+                                style={styles.maisProduto}
+                                source={require('../assets/imgMaisProduto.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.limparContainer}>
+                    <TouchableOpacity style={styles.apagarTudo}>
+                        <Image
+                            style={styles.lixo}
+                            source={require('../assets/lixo.png')}
+                        />
+                        <Text style={styles.apagarTudoTexto}>
+                            Limpar
+                        </Text>
+
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.containerValorTotal}>
+                    <View style={styles.valorTotal}>
+                        <View style={styles.resultado}>
+                            <Image
+                                style={styles.dinheiro}
+                                source={require('../assets/dinheiro.png')}
+                            />
+                            <Text style={styles.valorTotalTexto}>R$</Text>
+                        </View>
+                        <Text style={styles.valorTotalPreco}>0</Text>
+
+                    </View>
+
+                </View>
+
+                <StatusBar style="auto" />
+            </View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: '#F9F9F9'
+    },
+    header: {
+        backgroundColor: "#fff",
+        borderBottomEndRadius: 35,
+        borderBottomStartRadius: 35,
+        padding: 40,
+        borderWidth: 1,
+        borderColor: 'rgba(0, 0, 0, 0.16)'
+    },
+    headerInicio: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    profile: {
+        width: 50,
+        height: 50
+    },
+    sacola: {
+        width: 30,
+        height: 30
+    },
+    bemVindo: {
+        paddingTop: 20,
+        color: 'rgba(0, 0, 0, 0.62)',
+        fontFamily: "Inter",
+        fontWeight: "800",
+        fontSize: 26
+    },
+    ALLSpan: {
+        color: '#165515',
+        fontWeight: "800"
+    },
+    marketSpan: {
+        color: '#F7AB38',
+        fontWeight: "800"
+    },
+    dinheiro: {
+        width: 43,
+        height: 43,
+        marginRight: 15
+    },
+    input: {
+        width: 248,
+        height: 43,
+        paddingLeft: 20,
+        padding: 10, // Reduzido o padding para 10
+        borderWidth: 0.70,
+        borderColor: "#B8C8B7",
+        borderRadius: 25,
+        color: "#000",
+        fontFamily: "Inter",
+        alignItems: 'flex-start'
+    },
+    precoMax: {
+        flexDirection: "row",
+        paddingEnd: 40,
+        paddingStart: 40,
+        padding: 30,
+        alignItems: "center"
+
+    },
+    mercados: {
+        paddingTop: 15,
+        paddingEnd: 40,
+        paddingStart: 40,
+    },
+    mercadosTitulo: {
+        fontFamily: "Inter",
+        fontSize: 25,
+        fontWeight: "900",
+        color: "#165515",
+        paddingBottom: 15
+    },
+    adicionarMercado: {
+        flexDirection: "row",
+        width: 250,
+        padding: 6,
+        justifyContent: "space-evenly",
+        borderColor: 'rgba(22, 85, 21, 0.75)',
+        borderWidth: 1.5,
+        borderRadius: 25,
+        alignItems: "center",
+
+
+    },
+    adicionarMercadoTexto: {
+        fontFamily: "Inter",
+        fontWeight: "900",
+        fontSize: 17.5,
+        color: 'rgba(00, 00, 00, 0.62)'
+    },
+    adicionarMercadoSpan: {
+        color: "#165515"
+    },
+    adicionarIcon: {
+        width: 32,
+        height: 32,
+    },
+    cardContainer: {
+        padding: 40,
+        alignItems: "center"
+    },
+    imgCardAdicionar: {
+        width: 135,
+        height: 135,
+        alignSelf: "flex-end",
+        marginTop: 15
+    },
+    card: {
+        backgroundColor: "#165515",
+        height: 300,
+        width: 230,
+        borderRadius: 22,
+        shadowColor: '#000', // Cor da sombra
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        shadowOpacity: 0.6, // Opacidade da sombra (entre 0 e 1)
+        shadowRadius: 10, // Raio da sombra
+        elevation: 10,
+    },
+    botaoMais: {
+        marginEnd: '8%'
+    },
+    maisProduto: {
+        width: 60,
+        height: 60,
+        alignSelf: "flex-end",
+
+    },
+    tituloAdicionar: {
+        fontFamily: "Inter",
+        fontWeight: "800",
+        fontSize: 27,
+        color: "#F7AB38",
+        width: 129.8,
+        marginStart: "12%"
+    },
+    tituloAdicionarSpan: {
+        fontFamily: "Inter",
+        marginStart: "9%",
+        fontWeight: "800",
+        fontSize: 27,
+        color: "#FFf"
+    },
+    limparContainer: {
+        paddingStart: 40,
+
+    },
+    apagarTudo: {
+        flexDirection: "row",
+        borderWidth: 1.3,
+        borderColor: 'rgba(00, 00, 00, 0.44)',
+        padding: 10,
+        width: 120,
+        justifyContent: "space-around",
+        borderRadius: 25
+    },
+    apagarTudoTexto: {
+        fontFamily: "Inter",
+        fontSize: 17.3,
+        fontWeight: "800",
+        color: 'rgba(22, 85, 21, 0.85)'
+    },
+    lixo: {
+        width: 25,
+        height: 25
+    },
+    containerValorTotal: {
+        paddingStart: 40,
+        paddingEnd: 40,
+    },
+    valorTotal: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingEnd: "8%",
+        paddingStart: "8%",
+        height: 60,
+        backgroundColor: "#165515",
+        borderRadius: 15,
+        marginTop: 25,
+        marginBottom: 50
+    },
+    resultado: {
+        flexDirection: "row",
+        alignItems:"center"
+    },
+    valorTotalTexto: {
+        fontFamily: "Inter",
+        fontSize: 25,
+        fontWeight: "800",
+        color: "white",
+    },
+    valorTotalPreco: {
+        fontFamily: "Inter",
+        fontSize: 25,
+        fontWeight: "800",
+        color: "white",
+    }
+
+
 });
--- pages/produtos.js
+++ pages/produtos.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, TextInput } from 'react-native';
 
 export function Produtos() {
-  return (
-    <View style={styles.container}>
-      <Text>Cadastro de Produtos!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require('../assets/backgroundProdutos.png')} // Substitua pelo caminho da sua imagem
+                style={styles.background}
+            >
+                <View style={styles.containerForm}>
+
+                    <View style={styles.inputContainer}>
+                        <Image
+                            style={styles.sacolaBranca}
+                            source={require('../assets/sacolaBranca.png')}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Digite aqui..."
+                            placeholderTextColor="#999"
+                        />
+                    </View>
+                </View>
+
+
+            </ImageBackground>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    background: {
+        width: "100%",
+        flex: 1,
+        resizeMode: 'cover', // 'cover' para cobrir todo o contêiner
+        justifyContent: 'center', // Ajuste o conteúdo verticalmente no centro
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor:'rgba(22, 85, 21, 0.85)',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        fontSize: 16,
+    },
+    sacolaBranca: {
+        width: 24,
+        height: 24,
+        marginRight: 10,
+    },
 });
